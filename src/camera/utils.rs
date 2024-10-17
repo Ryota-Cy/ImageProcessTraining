d60b5881b34104675afd3ad50d87d955a88b99ba
Ryota-Cy <Ryota.Sai.net@gmail.com>
2024-10-17 10:10:22 +0900

    撮影パラメータに関して追加

--- src/camera/utils.rs
+++ src/camera/utils.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 // 3D座標を表す構造体
 struct Point3D {
     x: f64,
@@ -31,3 +33,28 @@ fn gaussian_lens_formula(object_distance: f64, image_distance: f64) -> f64 {
     focal_length
 }
 
+// 撮影画角を計算する関数
+fn calculate_aov(sensor_size: f64, focal_length: f64) -> f64 {
+    // 画角をラジアン(弧度法)で計算
+    let theta = 2.0 * (sensor_size / (2.0 * focal_length)).atan();
+    
+    // ラジアンを度に変換
+    theta * 180.0 / PI
+}
+
+// 35mm換算焦点距離を計算する関数
+fn calculate_35mm_equivalent(focal_length: f64, sensor_diagonal: f64) -> f64 {
+    // 35mmフルサイズセンサーの対角線長は43.3mm
+    let full_frame_diagonal = 43.3;
+
+    // 35mm換算焦点距離の計算
+    focal_length * (full_frame_diagonal / sensor_diagonal)
+}
+
+// シャッタースピードを計算
+fn calculate_shutter_speed(distance_to_subject: f64, subject_speed: f64, 
+    focal_length: f64, sensor_size: f64, pixel_count: u32) -> f64 {
+
+    let t_s = (sensor_size * distance_to_subject) / (subject_speed * focal_length * pixel_count as f64);
+    t_s
+}
